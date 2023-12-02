8825bf46a16d7ca2e7c2878ecbb77a942dbb71e3
Andres Morales <appandres993@gmail.com>
2023-12-02 10:18:26 -0500

    Instale la libreria de react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { FitnessProvider } from "./contexts/FitnessContext";
 import Home from "./pages/Home";
-import CreateRoutinePage from "./pages/CreateRoutinePage";
 
 const App = () => {
   return (
-    <FitnessProvider>
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/create-routine" component={CreateRoutinePage} />
-          {/* ...otros enrutamientos */}
-        </Switch>
-      </Router>
-    </FitnessProvider>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
   );
 };
 
