a379288528bed3ee6d67cd23a846e4e12d2b6a47
Andres Morales <appandres993@gmail.com>
2023-12-02 10:07:46 -0500

    Cree la estrutura para visualizar el en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { FitnessProvider } from "./contexts/FitnessContext";
+import Home from "./pages/Home";
+import CreateRoutinePage from "./pages/CreateRoutinePage";
 
-function App() {
-  return <></>;
-}
+const App = () => {
+  return (
+    <FitnessProvider>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/create-routine" component={CreateRoutinePage} />
+          {/* ...otros enrutamientos */}
+        </Switch>
+      </Router>
+    </FitnessProvider>
+  );
+};
 
 export default App;
